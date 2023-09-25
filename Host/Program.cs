1a170c6fb615ca276e93ef067747b49bb82848bf
Martin Zubovskiy <118899868+marz1010111@users.noreply.github.com>
2023-09-25 22:00:29 +0300

    updated

--- Host/Program.cs
+++ Host/Program.cs
@@ -2,63 +2,12 @@
 {
     internal class Program
     {
-        static int Main()
+        static void Main()
         {
-            Console.ForegroundColor = ConsoleColor.Cyan; // цвет консоли 
-
-            Model.ErrorCode myVariable = Model.ErrorCode.ConnectionLost;
-            Console.WriteLine("MyVariable = {0}", myVariable);
-
-            var today = Model.Days.Sunday;
-            int todayInt = (int)today;
-
-            Console.WriteLine("Today variable = {0}", today);
-            Console.WriteLine("TodayInt variable = {0}", todayInt);
-
-            int tomorrowInt = 2;
-            Model.Days tomorrowEnum = (Model.Days)tomorrowInt;
-
-            Console.WriteLine("tomorrowEnum variable = {0}", tomorrowEnum);
-
-            int intNine = 9;
-            Model.Days nineEnum = (Model.Days)intNine;
-
-            Console.WriteLine("nineEnum variable = {0}", nineEnum);
-
-            if (nineEnum == Model.Days.Saturday)
-            { 
-            }
-
-            if ((int)nineEnum == 32)
-            {
-            }
-
-            Console.WriteLine("Weekend = {0}", (Model.Days)96);
-
-            var lessonDays = Model.Days.Monday | Model.Days.Thursday;
-
-            Model.Days currentDay = Model.Days.Friday;
-
-            if (lessonDays.HasFlag(currentDay))
-            {
-                Console.ForegroundColor = ConsoleColor.Blue;
-                Console.WriteLine("Flag presented = {0}", currentDay);
-            }
-            else 
-            {
-                Console.ForegroundColor = ConsoleColor.Yellow;
-                Console.WriteLine("Flag not presented = {0}", currentDay);
-            }
-
-            ConsoleKeyInfo key = Console.ReadKey();
-
-            if (key.Key == ConsoleKey.Escape)
-            {
-                return 1;
-            }
-
-            return 0;
-
+            int a = 5;
+            int b = 2;
+            double c = a / (double)b;
+            Console.WriteLine($"C = {c}");
         }
     }
 }
