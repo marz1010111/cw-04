6fb17c777d9e124caaf0011fd530266b5906d700
Martin Zubovskiy <118899868+marz1010111@users.noreply.github.com>
2023-09-25 21:46:33 +0300

    updated

--- Host/Program.cs
+++ Host/Program.cs
@@ -2,11 +2,53 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine(Model.Constants.Greeting);
-           // Model.Constants.Greeting = ""; Ошибка, константа!
+            Console.ForegroundColor = ConsoleColor.Cyan; // цвет консоли 
 
+            Model.ErrorCode myVariable = Model.ErrorCode.ConnectionLost;
+            Console.WriteLine("MyVariable = {0}", myVariable);
+
+            var today = Model.Days.Sunday;
+            int todayInt = (int)today;
+
+            Console.WriteLine("Today variable = {0}", today);
+            Console.WriteLine("TodayInt variable = {0}", todayInt);
+
+            int tomorrowInt = 2;
+            Model.Days tomorrowEnum = (Model.Days)tomorrowInt;
+
+            Console.WriteLine("tomorrowEnum variable = {0}", tomorrowEnum);
+
+            int intNine = 9;
+            Model.Days nineEnum = (Model.Days)intNine;
+
+            Console.WriteLine("nineEnum variable = {0}", nineEnum);
+
+            if (nineEnum == Model.Days.Saturday)
+            { 
+            }
+
+            if ((int)nineEnum == 32)
+            {
+            }
+
+            Console.WriteLine("Weekend = {0}", (Model.Days)96);
+
+            var lessonDays = Model.Days.Monday | Model.Days.Thursday;
+
+            Model.Days currentDay = Model.Days.Friday;
+
+            if (lessonDays.HasFlag(currentDay))
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine("Flag presented = {0}", currentDay);
+            }
+            else 
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("Flag not presented = {0}", currentDay);
+            }
 
         }
     }
--- Model/Constants.cs
+++ Model/Constants.cs
@@ -1,7 +1,31 @@
 ﻿namespace Model
 {
-    public class Constants
+    public enum ErrorCode : byte
     {
-        public const string Greeting = "Hello, %username!";
+        None = 0,
+        Unknown = 1,
+        ConnectionLost = 200
+    }
+
+    [Flags]
+    public enum Days : int
+    {
+        None = 0,
+        Monday = 0b_0000_0001, // 1
+        Tuesday = 0b_0000_0010, // 2
+        Wednesday = 0b_0000_0100, // 4
+        Thursday = 0b_0000_1000, // 8
+        Friday = 0b_0001_0000, // 16
+        Saturday = 0b_0010_0000, // 32
+        Sunday = 0b_0100_0000, // 64
+        Weekend = Saturday | Sunday // 96
+
+        /*
+        0b_0010_0000
+        |
+        0b_0100_0000
+        ===
+        0b_0110_0000  // 32 + 64 = 96
+        */
     }
 }
