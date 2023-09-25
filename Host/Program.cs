5f09b98436c2f560de1172ea6264f502281c9e4e
Martin Zubovskiy <118899868+marz1010111@users.noreply.github.com>
2023-09-25 20:16:43 +0300

    if_else example

--- Host/Program.cs
+++ Host/Program.cs
@@ -6,8 +6,30 @@ namespace Host
     {
         static void Main(string[] args)
         {
-            Class1.Print();
-            Console.ReadKey();
+           // Class1.Print();
+           // Console.ReadKey();
+
+            var a = 120;
+
+            if (a == 1)
+            {
+                Console.WriteLine("1 case: 1");
+            }
+            else if (a > 500)
+            {
+                Console.WriteLine("2 case: > 500");
+            }
+            else if (a == 123)
+            {
+                Console.WriteLine("3 case: 123");
+            }
+            else 
+            {
+                Console.WriteLine("FallBack");
+            }
+
+
+
         }
     }
 }
