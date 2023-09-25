5ddf5c817998127dae9985fc43a3768f6d6e589e
Martin Zubovskiy <118899868+marz1010111@users.noreply.github.com>
2023-09-25 20:44:17 +0300

    Update Program.cs

--- Host/Program.cs
+++ Host/Program.cs
@@ -6,30 +6,58 @@ namespace Host
     {
         static void Main(string[] args)
         {
-           // Class1.Print();
-           // Console.ReadKey();
+            // && - условное И. если первое выражение фолс, то прирывает 
+            // & - бинарное И. требуется все выражения в условии 
 
-            var a = 120;
+            /*
+            0010 // 2
+            &
+            0110 // 6
+            ===
+            0010
+            */
 
-            if (a == 1)
+            /*
+            1100 // 12
+            &
+            1010 // 8
+            ===
+            1000 // 8
+            */
+
+            /*
+            1100  // 12
+            |  OR
+            1010  // 10
+            ===
+            1110  // 14
+            */
+
+            bool isHungry = false;
+
+            if (isHungry & GetAndCheckCake())
             {
-                Console.WriteLine("1 case: 1");
-            }
-            else if (a > 500)
-            {
-                Console.WriteLine("2 case: > 500");
-            }
-            else if (a == 123)
-            {
-                Console.WriteLine("3 case: 123");
-            }
-            else 
-            {
-                Console.WriteLine("FallBack");
+                Console.WriteLine("Задуваем свечи");
             }
 
+            Console.WriteLine("EXIT");
 
+            int a = 12;
+            int b = 10;
+            int resultAnd = a & b;
 
+            Console.WriteLine("Result And = " + resultAnd);
+
+            int resultOr = a | b;
+
+            Console.WriteLine("Result Or = " + resultOr);
+        }
+
+        static bool GetAndCheckCake()
+        {
+            Console.WriteLine("going...");
+
+            return true;
         }
     }
 }
