7a668289200a1177dfb767c804a4b042a9816a53
Martin Zubovskiy <118899868+marz1010111@users.noreply.github.com>
2023-09-25 19:57:12 +0300

    added project

---
+++ Host/Program.cs
@@ -0,0 +1,12 @@
+﻿using Model;
+
+namespace Host
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1.Print();
+        }
+    }
+}
---
+++ Model/Class1.cs
@@ -0,0 +1,10 @@
+﻿namespace Model
+{
+    public class Class1
+    {
+        public static void Print()
+        {
+            Console.WriteLine("Inside Print");
+        }
+    }
+}
